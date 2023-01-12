e7d0bde06a73ba1c9ae2c2ca7dbf682abd7dd58e
Milosz <milosz.sierakowski@wp.pl>
2023-01-12 12:29:17 +0100

    Change trello api root

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://task-prod-kodilla-tasks-9snr8l.mo4.mogenius.io/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const trelloApiRoot = 'https://api.trello.com/1';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
